9dc0b5c6e434942286d2c903cb8f20878ef2810b
haodong-neuro <hao.dong@yale.edu>
2026-02-12 13:55:04 -0500

    Delete meso-aux-scripts directory

--- meso-aux-scripts/generalizedAnalysisFunctions/averageContinuousDataToEventStructure.m
+++
@@ -1,9 +0,0 @@
-function averagedStructure = averageContinuousDataToEventStructure(dataTimestamps,data,eventTimestampStructure,preSamples,postSamples,parametricAxis)
-%% AVERAGECONTINUOUSDATATOEVENTSTRUCTURE averages continuous data of arbitrary dimensionality to a structure of events
-averagedStructure = struct;
-names = fields(eventTimestampStructure);
-for i =1:length(names)
-    averagedStructure.(names{i}) = averageContinuousDataToEventStructure(dataTimestamps,data,eventTimestampStructure.(names{i}),preSamples,postSamples,parametricAxis);
-end
-
-end
--- meso-aux-scripts/saveColumnwise.m
+++
@@ -1,6 +0,0 @@
-function saveColumnwise(data,path,prefix) %% save
-mkdir(path)
-for i = 1:size(data,2)   
-    column = squeeze(data(:,i,:));
-    save(fullfile(path,[prefix 'Col' num2str(i) '.mat']),"column");
-end
