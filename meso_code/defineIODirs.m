38d3e1f10553879c8220be210f4b3ee625b6dc32
haodong-neuro <hao.dong@yale.edu>
2026-02-12 14:04:19 -0500

    Add files via upload

---
+++ meso_code/defineIODirs.m
@@ -0,0 +1,17 @@
+%fixedInputDir = '/gpfs/gibbs/pi/cardin-higley/Rachel/';
+%fixedOutputDir = '/gpfs/gibbs/pi/cardin-higley/Rachel/';
+
+%fixedInputDir = '/vast/palmer/scratch/cardin/rlo4/';
+%fixedOutputDir = '/vast/palmer/scratch/cardin/rlo4/';
+
+fixedInputDir = '/vast/palmer/scratch/higley/hd362/HD_Mouse_Training';
+fixedOutputDir = '/vast/palmer/scratch/higley/hd362/HD_Mouse_Training';
+
+
+
+patchSize = 9;
+colPerBatch = 4;
+delay_length = 500;
+
+signalType = 'SigCol';
+
